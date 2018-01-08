83a0a29e4d69778337226a2944734f0518f2e726
wcc-miss <1209220150@qq.com>
2018-01-08 21:01:50 +0800

    ceshi

---
+++ src/main/java/com/example/wcc_csssdaw/WccCsssdawApplication.java
@@ -0,0 +1,12 @@
+package com.example.wcc_csssdaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WccCsssdawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WccCsssdawApplication.class, args);
+	}
+}
